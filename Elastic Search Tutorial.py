0f687829306b6b60b221bf215fad6d6b962e54c4
Rahul <77674219+Luhar42@users.noreply.github.com>
2023-09-05 16:22:42 -0400

    Create Elastic Search Tutorial.py to ensure Elastic Search is working

---
+++ Elastic Search Tutorial.py
@@ -0,0 +1,1 @@
+#To run Elastic Search Locally: docker run --rm -p 9200:9200 -p 9300:9300 -e "xpack.security.enabled=false" -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:8.9.0
