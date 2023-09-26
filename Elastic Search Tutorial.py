9c4451b9edc8fa6c76187f0b442364932bd6c46b
connorscholl295 <connorscholl295@gmail.com>
2023-09-26 12:08:00 -0400

    Our code now returns more than two or three results when searching for a given keyword, and the code now gathers course info from multiple pages of the api, not just page 1.

--- Elastic Search Tutorial.py
+++ Elastic Search Tutorial.py
@@ -4,9 +4,9 @@
 #Connect to ES Cluster
 from elasticsearch import Elasticsearch
 
-es = Elasticsearch("http://localhost:9200")
+# es = Elasticsearch('https://localhost:9200', ca_certs="http_ca.crt", basic_auth=("elastic", "0eCjuIZGIrFNI09vhq9_"))
+es = Elasticsearch('http://localhost:9200')
 es.info().body
-
 
 #Create an index for indexing documents
 mappings = {
@@ -32,26 +32,69 @@ if not es.indices.exists(index="courses"):
 import requests
 import json
 
-url = "https://content.osu.edu/v2/classes/search?q=#CSE&campus=col&term=2023"
+url = "https://content.osu.edu/v2/classes/search?q=cse&campus=col&term=1238"
 r = requests.get(url)
 
 #Check if the request to the API was successful or not
 if r.status_code == 200:
     json_data = r.json()
     
-    #Extract the courses section from the JSON data
-    courses_data = json_data.get("data", {}).get("courses", [])
-    #Iterate through the courses and index each document
-    for course in courses_data:
-        document = {
-            #"Class Group": course['course']['academicGroup'],
-            "Class Number": course['course']['catalogNumber'],
-            "Class Name": course['course']['title'],
-            "Class Description": course['course']['description'],
-            "Class Type": course['course']['academicCareer'],
-        }
-        #Index the document in Elasticsearch
-        es.index(index="courses", document=document)
-        print(f"Indexed document: {document}")
+    numberOfPages = json_data.get("data", {}).get("totalPages", int)
+    currentPageNum = 1
+    urlPrefix = "https://content.osu.edu/v2/classes/search?q=cse&campus=col&term=1238&p="
+    urlCurrent = urlPrefix + str(currentPageNum)
+
+    while currentPageNum <= numberOfPages:
+        print(urlCurrent)
+        r = requests.get(urlCurrent)
+        json_data = r.json()
+        #Extract the courses section from the JSON data
+        courses_data = json_data.get("data", {}).get("courses", [])
+        #Iterate through the courses and index each document
+        for course in courses_data:
+            document = {
+                #"Class Group": course['course']['academicGroup'],
+                "Class Number": course['course']['catalogNumber'],
+                "Class Name": course['course']['title'],
+                "Class Description": course['course']['description'],
+                "Class Type": course['course']['academicCareer'],
+            }
+            #Index the document in Elasticsearch
+            es.index(index="courses", document=document)
+            # print(f"Indexed document: {document}", file=open('output.txt', 'a'))
+        currentPageNum += 1
+        urlCurrent = urlPrefix + str(currentPageNum)
 else:
-    print(f"Failed to fetch data from the API. Status code: {response.status_code}")
+    print(f"Failed to fetch data from the API. Status code: {r.status_code}")
+
+#From this point, run the code in either a different file or different cell!
+
+# Define a parameter to search for classes with a specific keyword in the description
+searchParam = "software"
+
+courseQuery = {
+    "match": {
+        # "query" : searchParam,
+        # "fields" : ["Class Name", "Class Description"]
+        "Class Description" : searchParam
+    }
+}
+
+# Perform the search, using query rather than body since body is deprecated
+response = es.search(index="courses", query=courseQuery, size=100)  
+
+# Create a set to store unique class IDs
+unique_class_names = set()
+
+# Print search results without duplicates
+print("Got %d Hits:" % len(response['hits']['hits']))
+print("Search Results for", searchParam, "Classes:")
+for hit in response['hits']['hits']:
+    class_name = hit['_source']['Class Name']
+    if class_name not in unique_class_names:
+        class_description = hit['_source']['Class Description']
+        print(f"Class Name: {class_name}")
+        print(f"Class Description: {class_description}")
+        print("-----------------------")
+        # Add the class name to the set to mark it as processed
+        unique_class_names.add(class_name)
